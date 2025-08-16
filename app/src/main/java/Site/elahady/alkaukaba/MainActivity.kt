c66d8436bda9a12cbb2039ecc2dd09e3758e6403
it6masuyasby <it6.masuya.sby@gmail.com>
2025-08-16 15:02:18 +0700

    first commit

---
+++ app/src/main/java/Site/elahady/alkaukaba/MainActivity.kt
@@ -0,0 +1,11 @@
+package Site.elahady.alkaukaba
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
